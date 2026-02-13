dc7f6ea99314826621db0be8b05ab4e404d489bc
Boris0318 <borisesgal@gmail.com>
2026-02-12 18:22:10 -0600

    Richy Barber 2.1

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 // ===================================
 const BARBER_CONFIG = {
     // Business Name
-    businessName: 'Richy Barber',
+    businessName: 'Richy Barbers ',
     
     // Contact Information
     phone: '(555) 123-4567',
